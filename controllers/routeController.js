ed227d1a8a1667f3d8403f2519b0d1483afa6aa5
Aboubacar Zoungrana <aboubacarprofessional@yahoo.com>
2022-10-19 18:37:22 -0500

    bug fixed

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -1,17 +1,16 @@
 // Start our router
-// Import express 
+// Import express
 const express = require('express')
 // only the router none of the other app stuff
 const router = express.Router()
 const dataController = require('./dataController')
 const viewController = require('./viewController')
 
-
 // Routes
 // Index
 router.get('/', dataController.index, viewController.index)
 // New
-router.get('/new', viewController.newView )
+router.get('/new', viewController.newView)
 // Delete
 router.delete('/:id', dataController.destroy, viewController.redirectHome)
 // Update
@@ -22,7 +21,5 @@ router.post('/', dataController.create, viewController.redirectShow)
 router.get('/:id/edit', dataController.show, viewController.edit)
 // Show
 router.get('/:id', dataController.show, viewController.show)
-
-
 
 module.exports = router
--- models/fruit.js
+++ models/fruit.js
@@ -2,16 +2,15 @@ const mongoose = require('mongoose')
 
 // Make A Schema
 const fruitSchema = new mongoose.Schema({
-    name: { type: String, required: true},
-    color: { type: String, required: true},
-    readyToEat: Boolean
+  name: { type: String, required: true },
+  color: { type: String, required: true },
+  readyToEat: Boolean
 })
 
 // Make A Model From The Schema
 
 const Fruit = mongoose.model('Fruit', fruitSchema)
 
-
 // Export The Model For Use In The App
 
-module.exports = Fruit 
+module.exports = Fruit
